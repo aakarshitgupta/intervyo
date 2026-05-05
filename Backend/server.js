8bfbba0ea27a4527f4db21df7c55250f8122d52e
Akarshit <aakarshitgupta307@gmail.com>
2026-05-05 19:32:07 +0530

    fixed bug

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config()
+const { ProtocolError } = require("puppeteer")
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
